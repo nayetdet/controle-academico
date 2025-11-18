8839b16b13b17bbc39c2467830f3705b4e2dbbda
nayetdet <jpmoreiracampos21@gmail.com>
2025-11-18 15:55:18 +0000

    chore: finalizada configuração de criptografia do jwt e docker

--- src/main/java/br/edu/ifce/maracanau/controleacademico/config/SecurityConfig.java
+++ src/main/java/br/edu/ifce/maracanau/controleacademico/config/SecurityConfig.java
@@ -11,7 +11,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,6 +27,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
+@EnableMethodSecurity
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
